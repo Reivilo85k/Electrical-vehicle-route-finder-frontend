05db58f9ff2babf5f908ea6f29f1ee28ec789ec4
Olivier Laborde <laborde.olivier@gmail.com>
2021-02-14 20:29:22 +0200

    implemented signup and created map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthModule } from './auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {SignupRequestPayload} from "./signup-request.payload";
+import {AuthService} from "../shared/auth.service";
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  signupForm: FormGroup;
+  signupRequestPayload: SignupRequestPayload;
+
+  constructor(private authService: AuthService) {
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  signup() {
+    // @ts-ignore
+    this.signupRequestPayload.username = this.signupForm.get('username').value;
+    // @ts-ignore
+    this.signupRequestPayload.email = this.signupForm.get('email').value;
+    // @ts-ignore
+    this.signupRequestPayload.password = this.signupForm.get('password').value;
+
+    this.authService.signup(this.signupRequestPayload).subscribe(() => {
+      console.log('Signup Successful');
+    }, () => {
+      console.log('Signup Failed');
+    });
+  }
+
+}
