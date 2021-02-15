a21a59d1706ba436790f4742581cb50afe0892bd
Olivier Laborde <laborde.olivier@gmail.com>
2021-02-15 19:31:07 +0200

    login feature implemented, added  toastr, added local storage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {SignUpComponent} from "./auth/sign-up/sign-up.component";
 import {HomeComponent} from "./home/home.component";
 import {MapComponent} from "./map/map.component";
+import {LoginComponent} from './auth/login/login.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'map/:id', component: MapComponent},
-  { path: 'sign-up', component: SignUpComponent }
+  { path: 'sign-up', component: SignUpComponent },
+  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {SignupRequestPayload} from "./signup-request.payload";
 import {AuthService} from "../shared/auth.service";
+import {ToastrService} from "ngx-toastr";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sign-up',
@@ -13,7 +15,7 @@ export class SignUpComponent implements OnInit {
   signupForm: FormGroup;
   signupRequestPayload: SignupRequestPayload;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router, private toastr: ToastrService) {
     this.signupRequestPayload = {
       username: '',
       email: '',
@@ -34,12 +36,13 @@ export class SignUpComponent implements OnInit {
     this.signupRequestPayload.email = this.signupForm.get('email').value;
     this.signupRequestPayload.password = this.signupForm.get('password').value;
 
-    this.authService.signup(this.signupRequestPayload).subscribe(() => {
-      console.log('Signup Successful');
-    }, error => {
-      console.log(error);
-      console.log('Signup Failed');
-    });
+    this.authService.signup(this.signupRequestPayload)
+      .subscribe(() => {
+        this.router.navigate(['/login'],
+          { queryParams: { registered: 'true' } });
+      }, () => {
+        this.toastr.error('Registration Failed! Please try again');
+      });
   }
 
 }
