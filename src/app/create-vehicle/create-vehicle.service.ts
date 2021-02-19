e7f913287f7bf0e9fda3a47309f46056af9be6b4
Olivier Laborde <laborde.olivier@gmail.com>
2021-02-19 13:43:14 +0200

    added header bearer token to createVehicle httpClient

--- src/app/create-vehicle/create-vehicle.service.ts
+++ src/app/create-vehicle/create-vehicle.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {VehicleModel} from '../shared/vehicle-model';
 import {environment} from '../../environments/environment';
+import {AuthService} from '../auth/shared/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,12 @@ export class CreateVehicleService {
 
   baseUrl= environment.baseUrl
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private authService: AuthService) { }
 
   createVehicle(vehicleModel : VehicleModel): Observable<VehicleModel>{
+    let headers = new HttpHeaders().set('Authorization', 'Bearer '
+      + this.authService.getJwtToken());
     return this.httpClient.post<VehicleModel>(this.baseUrl + 'api/vehicle',
-      vehicleModel);
+      vehicleModel, {headers :  headers} );
   }
 }
