e97c27f954404aef97fa48a60fa287a1bdb59a9c
Olivier Laborde <laborde.olivier@gmail.com>
2021-02-27 18:34:15 +0200

    changed prod env url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://em9473.ev-route-finder.olivier-laborde.com/',
+  baseUrl: 'https://app.ev-route-finder.olivier-laborde.com/',
   openchargemaps: {
     API_KEY: '',
   }
